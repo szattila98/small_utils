cca563ee0dd12f73bcfee82e263a3a04c8ba5433
Szőke Attila <clownface68@gmail.com>
2022-06-20 13:36:21 +0200

    Rempref working base and denest skeleton added (#1)
    * Printing of files and expected output printing added.
    
    * Rename now only renames the file not the whole path.
    
    * Separated code into modules, added struct for handling rename tasks.
    
    * Started working on result error handling.
    
    * Added better error handling.
    
    * Code cleanup.
    
    * Reworked code structure for future extensibility.
    
    * Refactored renaming to better use PathBuf instead of string operations, and also refactored error display.
    
    * Replaced glob dependency to match file extensions.
    
    * Removed unnecessary print. Added relativized path getters.
    
    * Clippy fix.
    
    * Refactors and glob try-to-make-work session.
    
    * Removed glob as it was too restrictive and added walkdir.
    
    * Added recursive option,
    
    * Initialized denest.
    
    * Updated actions.

---
+++ rempref/src/cli.rs
@@ -0,0 +1,46 @@
+use std::fmt::Display;
+
+use crate::logic::{Config, FailedRemprefTask, RemPrefTask};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rempref")]
+/// A file prefix removal utility
+pub struct Args {
+    /// The length of the prefix to remove
+    #[structopt(short, long)]
+    pub prefix_length: u8,
+    /// Pass to make the file renames, otherwise it only does a dry run
+    #[structopt(short, long)]
+    pub do_renames: bool,
+    /// Specify file extensions to remove the prefix from
+    #[structopt(short, long)]
+    pub extensions: Vec<String>,
+    /// Recursively search all files in the working directory
+    #[structopt(short, long)]
+    pub recursive: bool,
+    // pub working_dir: Option<PathBuf> // custom working directory
+    // pub savepoint: bool, // save a csv with information to restore the renames
+    // pub load_savepoint: bool // restore the renames on failure
+    // pub file_pattern: Option<Patterns>, // basic patterns enum with custom option
+    // pub prefix_pattern: Option<String>, // eg remove prefixes that match this pattern
+    // pub similarities: bool // search for similarities between files and create tasks based on those
+}
+
+impl From<Args> for Config {
+    fn from(args: Args) -> Self {
+        Config::new(args.prefix_length, args.extensions, args.recursive)
+    }
+}
+
+impl Display for RemPrefTask {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} -> {}", self.from.display(), self.to.display())
+    }
+}
+
+impl Display for FailedRemprefTask {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} => {}", self.file_path.display(), self.reason)
+    }
+}
