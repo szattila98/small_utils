a4b1687343aeb0c86a22a0613b2a15ca81fc4321
Szőke Attila <clownface68@gmail.com>
2022-07-31 20:51:52 +0200

    Added commons library (#2)
    * Added commons member to the workspace
    
    * Created commons workspace member with functions that can be used elsewhere.

---
+++ commons/src/lib.rs
@@ -0,0 +1,45 @@
+use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
+
+pub use pathdiff::diff_paths;
+
+pub fn walkdir(root: &Path, depth: Option<usize>) -> Vec<PathBuf> {
+    let dir_iter = if let Some(depth) = depth {
+        WalkDir::new(root).max_depth(depth)
+    } else {
+        WalkDir::new(root)
+    };
+    dir_iter
+        .into_iter()
+        .filter(|entry| entry.is_ok())
+        .map(|entry| entry.unwrap().into_path())
+        .collect::<Vec<_>>()
+}
+
+pub fn read_files(root: &Path, depth: Option<usize>) -> Vec<PathBuf> {
+    walkdir(root, depth)
+        .into_iter()
+        .filter(|path| path.is_file())
+        .collect::<Vec<_>>()
+}
+
+pub fn read_dirs(root: &Path, depth: Option<usize>) -> Vec<PathBuf> {
+    walkdir(root, depth)
+        .into_iter()
+        .filter(|path| path.is_dir())
+        .collect::<Vec<_>>()
+}
+
+pub fn filter_by_extension(files: Vec<PathBuf>, extensions: &Vec<String>) -> Vec<PathBuf> {
+    if extensions.is_empty() {
+        return files;
+    }
+    files
+        .into_iter()
+        .filter_map(|file| {
+            extensions
+                .contains(&file.extension()?.to_string_lossy().to_string())
+                .then(|| file)
+        })
+        .collect::<Vec<_>>()
+}
